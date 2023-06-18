e94b0bba9cddbfe19e19012286cd1ae7d189b07f
minjaeleee <dlalswo4602@naver.com>
2023-06-18 23:10:13 +0900

    최장, 최단기록 표기

--- stop-watch/stop-watch-react/src/components/Laps.jsx
+++ stop-watch/stop-watch-react/src/components/Laps.jsx
@@ -3,11 +3,28 @@ import { timeFormatting } from '../utils/timeFormatting'
 
 export const Laps = ({laps}) => {
   const [lapTime, setLapTime] = useState(laps)
-
+  
   useEffect(()=>{
     setLapTime(prev => laps)
   },[laps])
   
+  const lapTimes = lapTime.length > 0 ? lapTime.map(el => el[1]) : [0]
+  const minRecord = Math.min(...lapTimes)
+  const maxRecord = Math.max(...lapTimes)
+  
+  const validateLapTimeColor = (record) => {
+    if(lapTime.length === 0) {
+      return "text-red-600"
+    }
+    if(record === maxRecord) {
+      return "text-red-600"
+    }
+    if(record === minRecord) {
+      return "text-green-600"
+    }
+    return ""
+  }
+
   return (
     <article className="text-gray-600 h-64 overflow-auto border-t-2">
     <ul>
@@ -15,10 +32,10 @@ export const Laps = ({laps}) => {
         lapTime.map((lap,idx) => 
           <li 
             key={ `${lap[0]} + ${idx} + ${lap[1]}` }
-            className="flex justify-between py-2 px-3 border-b-2"
+            className={`flex justify-between py-2 px-3 border-b-2 ${validateLapTimeColor(lap[1])}`}
           >
              <span>{`랩 ${lap[0]}`}</span> 
-             <span>{timeFormatting(lap[1])}</span> 
+             <span >{timeFormatting(lap[1])}</span> 
           </li>  
         )
       }
